211beef2179e63ecdf9e9c102b5c646260e00ec6
alexander.fridman <alexander.fridman@over-c.com>
2022-12-17 18:00:38 +0000

    eureka module code cleanup

--- Chapter04/eureka-service/src/main/java/com/packtpub/mmj/eureka/service/App.java
+++ Chapter04/eureka-service/src/main/java/com/packtpub/mmj/eureka/service/App.java
@@ -4,18 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- *
- * @author Sourabh Sharma
- */
 @SpringBootApplication
 @EnableEurekaServer
 public class App {
 
-    /**
-     *
-     * @param args
-     */
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
